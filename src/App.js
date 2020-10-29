272a9a937ac6e49edb8bbd4fa099721967c2c373
thobyv-kismat <vivee18@gmail.com>
2020-10-29 14:02:58 +0100

    add support for polygons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-
-//Polygons are coming soon!
+//Multipart Polygons are coming soon!
 
 const shapes = [
   { name: 'select a shape' },
@@ -33,17 +32,37 @@ const shapes = [
     name: 'polygon',
     attr: {
       name: 'polygon',
-      defaultVals: {},
-      data: ['cx', 'cy', 'rx', 'ry', 'fill']
+      defaultVals: { points: '250,175 100,261 99,88', fill: 'blue' },
+      data: ['points', 'fill']
     }
   },
-]
+];
+
+const generatePolygonPoints = (sideCount) => {
+  let radii = 100;
+  let centerX = 150;
+  let centerY = 175;
+  let pointsArray = [];
+  let pointsString = '';
+
+  for (var i = 1; i <= sideCount; i++) {
+    let pointX = Math.round(centerX +
+      radii * Math.cos(2 * Math.PI * i / sideCount));
+    let pointY = Math.round(centerY +
+      radii * Math.sin(2 * Math.PI * i / sideCount));
+    pointsArray.push(`${pointX},${pointY} `);
+  }
+  for (var i = 0; i <= pointsArray.length - 1; i++) {
+    pointsString += pointsArray[i];
+  }
+  return pointsString;
+}
 
 const hasKey = (obj, key) => {
   if (obj !== undefined || null) {
-    if (!obj.hasOwnProperty(key)) return false
+    if (!obj.hasOwnProperty(key)) return false;
   }
-  return true
+  return true;
 }
 
 
@@ -60,28 +79,36 @@ const DisplaySVG = ({ currentShapeData }) => {
       let attrs = {};
       for (let i = 0; i <= data.length - 1; i++) {
         let key = data[i];
-        attrs[key] = hasKey(currentShapeData, 'input') ? currentShapeData.input[key] : defaultVals[key];
+        attrs[key] = hasKey(currentShapeData, 'input') ?
+          currentShapeData.input[key] : defaultVals[key];
       }
-      console.log(attrs)
       return attrs;
     }
 
     return (
-      <svg width="300" height="300" version="1.1" xmlns="http://www.w3.org/2000/svg">
+      <svg width="350" height="350" version="1.1" xmlns="http://www.w3.org/2000/svg">
         <currentShapeData.attr.name {...svgAttrs()} />
-      </svg>)
+      </svg>
+    )
   }
 
-  return (<b>no shape</b>)
+  return (<div className="centered"><b>no shape yet</b></div>)
 }
 
 const InputFields = ({ currentShapeData, setSelectedShape }) => {
   const [inputValue, setInputValue] = useState(0);
   const { name } = currentShapeData;
 
+  const parseValue = (v, k) => {
+    if (k == 'points') {
+      return generatePolygonPoints(v);
+    }
+    return v;
+  }
+
   const handleInput = (val, key) => {
-    setInputValue({ ...inputValue, [key]: (key !== 'fill' ? parseInt(val) : val) })
-    setSelectedShape({ ...currentShapeData, input: { ...currentShapeData.input, [key]: val } })
+    setInputValue({ ...inputValue, [key]: val })
+    setSelectedShape({ ...currentShapeData, input: { ...currentShapeData.input, [key]: parseValue(val, key) } })
   }
 
   return (
@@ -90,12 +117,12 @@ const InputFields = ({ currentShapeData, setSelectedShape }) => {
         name === 'rectangle' &&
         <>
           <h2> W: </h2>
-          <input className="inputs" type="number" value={inputValue.width} onChange={({ target }) =>
+          <input className="inputs" type="number" onChange={({ target }) =>
             handleInput(target.value, 'width')
           } />
 
           <h2> H: </h2>
-          <input className="inputs" type="number" value={inputValue.height} onChange={({ target }) =>
+          <input className="inputs" type="number" onChange={({ target }) =>
             handleInput(target.value, 'height')
           } />
 
@@ -106,7 +133,7 @@ const InputFields = ({ currentShapeData, setSelectedShape }) => {
         name === 'circle' &&
         <>
           <h2> radius </h2>
-          <input className="inputs" type="number" value={inputValue.r} onChange={({ target }) =>
+          <input className="inputs" type="number" onChange={({ target }) =>
             handleInput(target.value, 'r')
           } />
 
@@ -117,13 +144,24 @@ const InputFields = ({ currentShapeData, setSelectedShape }) => {
         name === 'ellipse' &&
         <>
           <h2> r1: </h2>
-          <input className="inputs" type="number" value={inputValue.rx} onChange={({ target }) =>
+          <input className="inputs" type="number" onChange={({ target }) =>
             handleInput(target.value, 'rx')
           } />
 
           <h2> r2: </h2>
-          <input className="inputs" type="number" value={inputValue.ry} onChange={({ target }) =>
+          <input className="inputs" type="number" onChange={({ target }) =>
             handleInput(target.value, 'ry')
+          } />
+
+        </>
+      }
+
+      {
+        name === 'polygon' &&
+        <>
+          <h2> number of sides: </h2>
+          <input className="inputs" type="number" onChange={({ target }) =>
+            handleInput(target.value, 'points')
           } />
 
         </>
@@ -131,7 +169,7 @@ const InputFields = ({ currentShapeData, setSelectedShape }) => {
 
 
       <h1> Enter color name </h1>
-      <input className="inputs" type="text" value={inputValue.fill} onChange={({ target }) =>
+      <input className="inputs" type="text" onChange={({ target }) =>
         handleInput(target.value, 'fill')
       } />
 
@@ -139,18 +177,42 @@ const InputFields = ({ currentShapeData, setSelectedShape }) => {
   )
 }
 
-const App = () => {
+const enteredShapes = [];
 
+const EnteredShapes = ({ shapesArr }) => {
+  if (shapesArr) return shapesArr.map(name =>
+    <div className="tag" key={name}>
+      <b>{name}</b>
+    </div>)
+
+  return null;
+}
+
+const App = () => {
   const [selectedShape, setSelectedShape] = useState('');
+  const [savedShapes, setSavedShapes] = useState([]);
+
+  const saveShapeToLocalStorage = (shapeName) => {
+    if (!savedShapes.find(name => name === shapeName)) {
+      enteredShapes.push(shapeName);
+      localStorage.setItem('selected_shapes', JSON.stringify(enteredShapes));
+    }
+  }
+
+  const getShapesFromLocalStorage = () => {
+    return JSON.parse(localStorage.getItem('selected_shapes'));
+  }
 
   const setShape = (_name) => {
-    let shape = shapes.find(({ name }) => name === _name)
+    saveShapeToLocalStorage(_name);
+    let shape = shapes.find(({ name }) => name === _name);
     return { ...shape, input: shape.attr.defaultVals }
   }
 
   useEffect(() => {
-    console.log(selectedShape)
-  }, [selectedShape])
+    setSavedShapes(getShapesFromLocalStorage());
+  }, [selectedShape]);
+
 
   return (<div className="wrapper">
     <div>
@@ -173,12 +235,19 @@ const App = () => {
           />
 
         </div>
+
         <div className="view">
-          <DisplaySVG currentShapeData={selectedShape} />
+          <div className="box">
+            <DisplaySVG currentShapeData={selectedShape} />
+          </div>
+          <div className="tags">
+            <h5> Entered shapes: </h5  >
+            <EnteredShapes shapesArr={savedShapes} />
+          </div>
         </div>
       </div>
     </div>
-  </div>)
+  </div >)
 
 }
 
